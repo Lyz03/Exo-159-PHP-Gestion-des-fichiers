33560ca09dbcbdfa813b577781c7ae9bb598fb0b
Lyz03 <lizoe@netcourrier.com>
2022-01-17 16:32:02 +0100

    exo 159

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
  * 2. Afficher le contenu du fichier dans un div
  */
 // TODO Votre code ici.
+$content = file_get_contents("lire.txt");
+
+if ($content) {
+    echo "<div>$content</div>";
+}
 
 /**
  * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
@@ -12,14 +17,30 @@
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
+$file = fopen('lire.txt', 'rb');
 
+while ($line = fgets($file)) {
+    echo $line . "<br>";
+}
+
+fclose($file);
 
 /**
  * 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
+$file = fopen('lire.txt', 'rb');
+$array = [' ', '.', '?', '!'];
 
+while ($letter = fgetc($file)) {
+    if (!in_array($letter, $array)) {
+        echo $letter;
+    }
+
+}
+
+fclose($file);
 
 /**
  * 6. Ne cherchez pas la logique dans cet exo ^^
